ebd464c86b112e60e491786e6fcf25ab1c345469
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2024-02-14 10:09:27 +0400

    Initial commit

---
+++ CSVCharts/CSVChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSVChartsApp.swift
+//  CSVCharts
+//
+//  Created by Anton on 14.02.24.
+//
+
+import SwiftUI
+
+@main
+struct CSVChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
